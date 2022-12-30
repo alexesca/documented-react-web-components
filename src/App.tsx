c5537f91f177943e5644529a071e97f8270a9721
alexesca <alexescamore@gmail.com>
2022-12-30 11:36:41 -0700

    Added counter button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Counter from './counter/button';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -17,9 +16,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Counter></Counter>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
